fb2b6a99c1491f60e0646b577297342b285d73cc
andreivercosa <andreivercosa@gmail.com>
2019-03-12 19:52:03 -0400

    v1

---
+++ MarginApp/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MarginApp
+{
+    
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+           
+        }
+
+        void equilibrio(object sender, System.EventArgs e)
+        {
+
+            int i = Convert.ToInt32(slider.Value);
+            if (i == 0 || i == 360)
+            {
+                rodante.Text = "-- Estavel --";
+            }
+            else
+            {
+                rodante.Text = "-- Girando --";
+            }
+            label_valueSlider.Text = i.ToString();
+        }
+
+
+
+    }
+}
